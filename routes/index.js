cca2096451ca25745ecb310268bd6d30b8186252
rickyall <783814127@qq.com>
2016-03-17 17:14:50 +0800

    添加正负面训练

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    News.findAll({where:{servicetype:0,rate:-1},limit:1000,order: 'publishtime DESC'}).then(function(news){
+    News.findAll({where:{servicetype:2},limit:1000,order: 'publishtime DESC'}).then(function(news){
       res.render('index',{
         news:news,
         title:'训练鹰眼'
       });
     });
+});
+
+//修正正负面
+router.get('/:id/correctNews/:type',function(req,res){
+  var id = req.params.id;
+  var type = parseInt(req.params.type);
+  News.findOne({where:{id:id}}).then(function(news){
+    news.adjust_bond_rate = type;
+    news.save();
+  }).catch(function(err){
+    console.log(err);
+  });
 });
 
 module.exports = router;
