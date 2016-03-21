b5fdc52d730920017dfa251a5ed3b77a2412a1d0
rickyall <783814127@qq.com>
2016-03-21 14:54:33 +0800

    更新

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    News.findAll({where:{servicetype:2},limit:1000,order: 'publishtime DESC'}).then(function(news){
+    News.findAll({where:{servicetype:2},limit:10,order: 'publishtime DESC'}).then(function(news){
       res.render('index',{
         news:news,
         title:'训练鹰眼'
